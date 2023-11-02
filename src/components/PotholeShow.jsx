fbd8c3fe0a3a6c6828d44eb6423b872c11d3a9b0
Geeta <geetkiroula1999@gmail.com>
2023-11-02 11:48:09 +0530

    video

--- src/components/PotholeShow.jsx
+++ src/components/PotholeShow.jsx
@@ -3,16 +3,19 @@ import React from "react";
 const PotholeShow = (props) => {
   return (
     <div className="h-[20rem] w-80 bg-white p-4 rounded-lg flex flex-col grow ">
-      <div className="flex flex-col">
-        <strong className="text-gray-700 font-medium">Location</strong>
-        <span className="text-gray-300 text-xs font-semibold">
-          City | State
-        </span>
-      </div>
 
-      <div className="w-4/5 ml-8 text-xs ">
+      <div className="w-full grid grid-cols-2 gap-4 my-2">
         {/* <ResponsiveContainer width="100%" height="100%"> */}
-          <img className="py-5 w-60 h-[15rem]" src = {props.src}></img>
+         <div className="flex-col">
+            <strong className="text-blue-700 font-medium">Original</strong>
+            <img className="py-2 w-30 h-[15rem]" src = {props.src1}></img>
+         </div>
+          
+         <div className="flex-col">
+            <strong className="text-blue-700 font-medium">Detected</strong>
+            <img className="py-2 w-30 h-[15rem]" src = {props.src2}></img>
+         </div>
+         
         {/* </ResponsiveContainer> */}
       </div>
     </div>
